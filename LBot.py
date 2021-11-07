988bb4d2249e26eb939bec8f9d9f966855f8539f
FelipeReis11011 <58182237+FelipeReis11011@users.noreply.github.com>
2021-11-07 01:42:32 -0200

    A Whole Rework in LBot
    Changed almost everything in the code

--- LBot.py
+++ LBot.py
@@ -1,78 +1,59 @@
 import srcomapi
+import requests
+import os
 from time import sleep
 from time import time
-from textos import embelezeTempo as eT
 
 # Util Section #
 
 def apiSleep(api,text):
     begin = time()
-    data = api.get(text)
+    data = requests.get(text).json()["data"]
     end = time()
     timeElapsed = end-begin
     if timeElapsed < 0.6:
         sleep(0.6-timeElapsed)
     return data
 
-def getList(name):
-    file = open(DIRECTORY + "\\" + name + ".csv",'r')
-    returnList = []
-    line = file.readline()[:-1]
-    returnList.append(line.split(";")[0])
-    while line:
+def getIDs():
+    with open(DIRECTORY + "\\users.csv",'r') as file:
+        data = {}
         line = file.readline()[:-1]
-        if line:
-            returnList.append(line.split(";")[0])
-    file.close()
-    return returnList
+        while line:
+            runner,Id,_ = line[:-1].split(";")
+            data[runner] = Id
+            line = file.readline()
+    return data
 
-def userId(user,api):
-    file = open(DIRECTORY + "\\users.csv",'r')
-    data = file.readline()[:-1].split(";")
-    if data[0].lower() == user.lower():
-        file.close()
-        return data[1]
-    while data[0]:
-        data = file.readline()[:-1].split(";")
-        if data[0] == user:
-            file.close()
-            return data[1]
-
-def writeLog(text,user,info):
-    file = open(DIRECTORY + "\\log.txt",'a')
-    file.write(" : ".join([text,user,info]))
-    file.write("\n")
-    print(" : ".join([text,user,info]))
-    file.close()
-
-def getFlag(user):
-    if user in ["(guest) N/A","(guest) n/a"]:
-        return ":united_nations:"
-    if user == "Silo_Simon":
-        return "(deleted) :flag_us:"
-    file = open(DIRECTORY + "\\users.csv",'r')
-    data = file.readline()[:-1].split(";")
-    if data[0].lower() == user.lower():
-        file.close()
-        return data[2]
-    while data[0]:
-        data = file.readline()[:-1].split(";")
-        if data[0].lower() == user.lower():
-            file.close()
-            return data[2]
-
-def getLog(user,info):
-    file = open(DIRECTORY + "\\log.txt",'r')
-    line = file.readline()[:-1]
-    while line:
-        line = line.split(" : ")
-        if user == line[1]:
-            if line[0] == info:
-                file.close()
-                return int(line[2])
+def getFlags():
+    with open(DIRECTORY + "\\users.csv",'r') as file:
+        data = {}
         line = file.readline()[:-1]
-    file.close()
-    return False
+        while line:
+            runner,_,flag = line[:-1].split(";")
+            data[runner] = flag
+            line = file.readline()
+    data["Silo_Simon"] = "(deleted) :flag_us:"
+    return data
+
+def writeCache(title,data):
+    with open(DIRECTORY + "\\cache.csv",'r') as file:
+        for runner in data:
+            file.write(";".join([title,runner,data[runner]]))
+            file.write("\n")
+
+def getCache():
+    dataGames = {}
+    dataRuns = {}
+    dataWrs = {}
+    datas = {"games":dataGames,"runs":dataRuns,"wrs":dataWrs}
+    if "cache.csv" in os.listdir(DIRECTORY):
+        with open(DIRECTORY + "\\cache.csv",'r') as file:
+            line = file.readline()[:-1]
+            while line:
+                info,runner,number = line.split(";")
+                datas[info][runner] = number
+    return [dataGames,dataRuns,dataWrs]
 
 # API Section #
 
@@ -88,57 +69,47 @@ def runsByGuestNA():
     return total
 
 def lbData(api):
-    datawrs = {}
-    datagames = {}
-    dataruns = {}
-    for runner in getList("users"):
-        data = getLog(runner,"wrs")
-        runnerID = userId(runner,api)
-        if data:
-            datawrs[runner] = int(data)
-            print("[LOG]{0} : {1} : {2}".format("wrs",runner,data))
-        else:
-            runs = apiSleep(api,"users/{}/personal-bests?top=1".format(runnerID))
-            writeLog("wrs",runner,str(len(runs)))
-            datawrs[runner] = len(runs)
-
-        searchNeeded = False
-        
-        data = getLog(runner,"runs")
-        if data:
-            dataruns[runner] = data
-        else:
-            searchNeeded = True
-
-        data = getLog(runner,"games")
-        if data:
-            datagames[runner] = data
-        else:
-            searchNeeded = True
-        
-        if searchNeeded:
-            gamesPlayed = []
-            total = 0
-            offset = 0
-            while True:
-                d = apiSleep(api,"runs?user={0}&offset={1}&max=200&status=verified".format(runnerID,offset*200))
-                total += len(d)
-                for run in d:
-                    if run["game"] not in gamesPlayed:
-                        gamesPlayed.append(run["game"])
-                if len(d) < 200:
-                    break
-                offset += 1
-            writeLog("runs",runner,str(total))
-            writeLog("games",runner,str(len(gamesPlayed)))
-            
-            dataruns[runner] = total
-            datagames[runner] = len(gamesPlayed)
-        else:
-            print("[LOG]{0} : {1} : {2}".format("runs",runner,dataruns[runner]))
-            print("[LOG]{0} : {1} : {2}".format("games",runner,datagames[runner]))
-    datagames["Silo_Simon"] = 123
-    return (datawrs,datagames,dataruns)
+    dataGames, dataRuns, dataWrs = getCache()
+    try:
+        runnerIDs = getIDs()
+        for runner in runnerIDs:
+            runnerID = runnerIDs[runner]
+            if runner in dataWrs:
+                print("[cache]{0} : {1} : {2}".format("wrs",runner,data))
+            else:
+                runs = apiSleep(api,"users/{}/personal-bests?top=1".format(runnerID))
+                print(" : ".join(["wrs",runner,str(len(runs))]))
+                dataWrs[runner] = len(runs)
+            if not((runner in dataRuns) and (runner in dataGames)):
+                gamesPlayed = []
+                totalRuns = 0
+                offset = 0
+                while offset*200 < 10000:
+                    d = apiSleep(api,"runs?user={0}&offset={1}&max=200&status=verified".format(runnerID,offset*200))
+                    totalRuns += len(d)
+                    for run in d:
+                        if run["game"] not in gamesPlayed:
+                            gamesPlayed.append(run["game"])
+                    if len(d) < 200:
+                        break
+                    offset += 1
+                if offset*200==10000:
+                    print("implement backwards search for runner : " + runner)
+                    raise Exception("implement backwards search for runner : " + runner)
+                print(" : ".join(["runs",runner,str(totalRuns)]))
+                print(" : ".join(["games",runner,str(len(gamesPlayed))]))
+                
+                dataRuns[runner] = totalRuns
+                dataGames[runner] = len(gamesPlayed)
+            else:
+                print("[cache]{0} : {1} : {2}".format("runs",runner,dataRuns[runner]))
+                print("[cache]{0} : {1} : {2}".format("games",runner,dataGames[runner]))
+        dataGames["Silo_Simon"] = 123
+        return (dataWrs,dataGames,dataRuns)
+    except:
+        writeCache("wrs",dataWrs)
+        writeCache("runs",dataRuns)
+        writeCache("games",dataGames)
 
 def lbOrder(data):
     lbNumbers = list(data.values())
@@ -156,6 +127,7 @@ def lbOrder(data):
 
 def makeLb(data):
     lbRunners,lb = lbOrder(data)
+    flags = getFlags()
     position = 1
     tiedPos = 0
     text = ""
@@ -166,25 +138,19 @@ def makeLb(data):
                 tiedPos += 1
             else:
                 tiedPos = 0
-        line = "{0}. {1} {2} - {3}".format(position - tiedPos,runner,getFlag(runner),lb[runner])
+        line = "{0}. {1} {2} - {3}".format(position - tiedPos,runner,flags[runner],lb[runner])
         print(line)
         text += line + "\n"
         position += 1
-    file = open(DIRECTORY + "\\results.txt",'a')
-    file.write(text)
-    file.close()
+    with open(DIRECTORY + "\\results.txt",'a') as file:
+        file.write(text)
     return text
 
 DIRECTORY = "C:\\Users\\Programador\\Documents\\GitHub\\SrcLbMaker\\SrcLbMaker"
-a = time()
-api = srcomapi.SpeedrunCom();
-api.debug = 0
 datas = lbData(api)
 for n,data in enumerate(datas):
     print(["\nWRS:\n","\nGAMES:\n","\nRUNS\n"][n])
     if n==2:
-        print("guest : "+str(runsByGuestNA()))
+        print("-. N/A(guest) :united_nations: : "+str(runsByGuestNA()))
     makeLb(data)
     print()
-b = time()
-print(eT(b-a))
