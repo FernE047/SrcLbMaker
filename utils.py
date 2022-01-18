2dfc65262531b57d6a7b81acfaacc9b5c6d5720e
Rayu1 <75923279+Rayu1@users.noreply.github.com>
2022-01-18 20:09:01 +0600

    Edited runs function

--- utils.py
+++ utils.py
@@ -16,16 +16,15 @@ def wrs(userid):
 
 def runs(userid):
     """Return a user's runs count."""
-    done = False
     offset = 0
 
-    while not done:
+    while True:
         data = requests.get(
             f"{API}runs?user={userid}&max=200&offset={offset * 200}"
         ).json()
         offset += 1
         if data["pagination"]["size"] < 200:
-            done = True
+            break
 
     return data["pagination"]["offset"] + data["pagination"]["size"]
 
@@ -39,13 +38,6 @@ def gp(userid):
     return len(set([i["run"]["game"] for i in data["data"]]))
 
 
-def mods(userid):
-    """Return a number of games moderated by user."""
-    return requests.get(
-        f"{API}games?moderator={userid}&_bulk=yes&max=1000"
-    ).json()["pagination"]["size"]
-
-
 def cp(userid):
     """Return a number of categories in which the user has runs."""
     data = requests.get(
@@ -53,6 +45,13 @@ def cp(userid):
     ).json()
 
     return len(set([i["run"]["category"] for i in data["data"]]))
+
+
+def mods(userid):
+    """Return a number of games moderated by user."""
+    return requests.get(
+        f"{API}games?moderator={userid}&_bulk=yes&max=1000"
+    ).json()["pagination"]["size"]
 
 
 def pod(userid):
